9d59f248e572812c9e3a257a51aa7ac18a3bd05f
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-09-04 20:56:25 -0300

    Update WCARG.bat

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -1,4 +1,4 @@
-set URL=[https://www.eventbrite.com/,https://www.ole.com.ar/]
+set URL=https://www.eventbrite.com/,https://www.ole.com.ar/
 ECHO %URL%
 git clone https://github.com/Yosoyquemero/WCARG.git
 cd WCARG
