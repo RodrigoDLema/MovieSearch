56280cb4dfaf4daa8ba67b69e22b11bad8f12157
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-09-04 19:10:08 -0300

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -8,3 +8,5 @@ echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
+
+git clone https://github.com/Yosoyquemero/MovieSearch.git
