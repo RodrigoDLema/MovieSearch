73aaded38ff290f3e27b47b2e83b2b4938843354
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-09-04 18:57:11 -0300

    Update WCARG.bat

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -1,6 +1,5 @@
-git clone https://github.com/Yosoyquemero/WCARG.git
-//git 'https://github.com/GonzaloFuentes/WCARG.git'
+git clone 'https://github.com/Yosoyquemero/WCARG.git'
+
 npm install
-//bat 'set @URLWCAG="https://www.eventbrite.com/"'
-//bat 'echo $URLWCAG'
+
 node index.js https://www.eventbrite.com/
