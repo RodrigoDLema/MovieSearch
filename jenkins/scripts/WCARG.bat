7062bcf571f47d9ddf56175a54ba4a5df85863a9
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-09-04 22:41:50 -0300

    Update WCARG.bat

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -1,5 +1,5 @@
 set URL="https://www.eventbrite.com/ ,https://www.ole.com.ar/";
-git clone https://github.com/Yosoyquemero/WCARG.git
+git clone https://github.com/AccesibilidadWCARG/WCARG.git
 cd WCARG
 call npm install
 node index.js %URL%
