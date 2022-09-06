a274f3e5a656decb326ee0249efb06712cd18a7c
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-09-05 23:33:50 -0300

    Update WCARG.bat

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -2,4 +2,4 @@ set URL="https://www.eventbrite.com/ ,https://www.musimundo.com/";
 git clone https://github.com/AccesibilidadWCARG/WCARG.git
 cd WCARG
 call npm install
-node index.js %URL%
+node wcarg %URL%
