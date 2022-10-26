006f639559a50c6da6e8fdf274ada0d445811830
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-10-25 22:28:40 -0300

    Update WCARG.bat
    EMAIL UPDATED

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -1,4 +1,5 @@
 set URL="https://www.eventbrite.com/ ,https://www.musimundo.com/"
+set EMAIL="gonza.a.fuentes@gmail.com;rlema.1989@gmail.com"
 git clone https://github.com/AccesibilidadWCARG/WCARG.git
 cd WCARG
 call npm install
