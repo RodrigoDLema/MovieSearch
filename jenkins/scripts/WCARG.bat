dea96fa2a62ad131933ec9550cfbdeb2f7d68897
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-10-25 23:56:13 -0300

    Update WCARG.bat
    email

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -3,4 +3,4 @@ set EMAIL="gonza.a.fuentes@gmail.com;rlema.1989@gmail.com"
 git clone https://github.com/AccesibilidadWCARG/WCARG.git
 cd WCARG
 call npm install
-node wcarg %URL% %EMAIL%
+node wcarg %URL% "gonza.a.fuentes@gmail.com;rlema.1989@gmail.com"
