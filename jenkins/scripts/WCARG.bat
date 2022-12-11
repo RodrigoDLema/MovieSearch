2b5afa8b92c9b1be748d65b042404a35c557cc50
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-12-10 21:54:46 -0300

    Update WCARG.bat

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -1,4 +1,4 @@
-set URL="http://localhost:3000/free/dashboard/default/"
+set URL="http://localhost:3000/"
 set EMAILS="gonza.a.fuentes@gmail.com;rlema.1989@gmail.com"
 git clone https://github.com/AccesibilidadWCARG/WCARG.git
 cd WCARG
